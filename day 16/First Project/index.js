7a943f3c7737af971590586bbace49f3de8d843e
Het-Nakrani-04 <nakaranihet18@gmail.com>
2026-05-26 13:27:30 +0530

    hyy

--- day 16/First Project/index.js
+++ day 16/First Project/index.js
@@ -6,5 +6,20 @@ app.listen(3000, ()=> {
 })
 
 app.get('/',(req, res) => {
-    res.send("hello world");
+    res.send("<h1>hello world</h1>");
+})
+
+app.get('/about/:userid/book/:bookid',(req, res) => {
+    res.send(req.params.bookid);
+})
+
+app.get('/Gallery',(req, res) => {
+    res.send("<h1>This is Gallery</h1>");
+})
+
+app.get('/search',(req, res) =>{
+    const name = req.query.name
+    const age = req.query.age
+
+    res.send(`Csearch result for name : ${name},age: ${age}`)
 })
